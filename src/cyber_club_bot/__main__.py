9de0c6a6a47f466a1f49dd9dae14f469faae0755
Jerry Arciaga <jerryarciaga11@gmail.com>
2025-06-06 11:35:09 -0700

    Fix: Move import line to __main__.py

--- src/cyber_club_bot/__main__.py
+++ src/cyber_club_bot/__main__.py
@@ -1,24 +1,4 @@
-from dotenv import load_dotenv
-import os
-import discord
-from discord.ext import commands
-
-load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-
-description = '''A Discord bot that automates Cybersecurity club functions.'''
-
-bot = commands.Bot(command_prefix='!', description=description,
-                   intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
-
+from .discord_bot import *
 
 if __name__ == "__main__":
     bot.run(DISCORD_TOKEN)
